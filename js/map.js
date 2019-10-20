155e78aa23ce558a5a2a68d190faafc95a08d6a6
DanDev2519 <54775982+DanDev2519@users.noreply.github.com>
2019-10-20 19:14:39 +0300

    Оживил интерактивные элементы
    Добавил карту. Оживил форму обратной связи. Оживил модальное окно в каталоге.

---
+++ js/map.js
@@ -0,0 +1,24 @@
+// Оживление окна с картой
+var mapLink = document.querySelector(".image-map-link");
+
+var mapPopup = document.querySelector(".map-popup");
+var mapClose = mapPopup.querySelector(".btn-close");
+
+mapLink.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
